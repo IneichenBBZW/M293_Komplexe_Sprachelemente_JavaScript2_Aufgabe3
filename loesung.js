715f205c156f838ed88081f986ef27159b150354
IneichenBBZW <98276716+IneichenBBZW@users.noreply.github.com>
2024-05-21 16:59:38 +0200

    Add files via upload

---
+++ loesung.js
@@ -0,0 +1,13 @@
+function gerade() {
+  let summe = 0;
+
+  for (let i = 0; i < 900000000; i++) {
+    if (i % 2 === 0) {
+      summe += i;
+    }
+  }
+
+  return summe;
+}
+
+console.log(gerade());
